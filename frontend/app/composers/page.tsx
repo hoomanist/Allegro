9b09a690af25c0f87a92f2f22d3c42b985d00225
Hooman Hedayati <its.hoomanist@gmail.com>
2023-09-07 14:12:40 +0330

    don't ask what i've done

--- frontend/app/composers/page.tsx
+++ frontend/app/composers/page.tsx
@@ -1,7 +1,6 @@
 import Card from '../../components/card'
-import {FiMic} from "@react-icons/all-files/fi/FiMic"
 async function getData() {
-    const res = await fetch("http://127.0.0.1:8001/api/q/composers")
+    const res = await fetch("http://127.0.0.1:8001/api/q/composers", {cache: 'no-cache'})
     if (!res.ok) {
         throw new Error("unable to fetch data")
     }
@@ -15,17 +14,21 @@ export default async function Composer() {
 
     console.log(data[0]["name"])
     return (
-        <div className="grid grid-cols-4 ">
+        <div className="grid grid-cols-5 gap-x-3 gap-y-2 ml-20 mt-10 mr-5 z-[-1] justify-center">
             {data.map(item => (
+            <div className="scale-[0.9] rounded-xl shadown-lg border-black bg-gradient-to-t from-zinc-200 to-amber-100 group">
             <Card imgSrc={imageUrl + item["photo"]}>
-            <div >
-                <h3 className='text-xl font-bold mb-2'>{item["name"]}</h3>
+            </Card>
+            <div className="m-2 items-center ">
+                <h3 className="text-xl font-semibold mb-2">{item["name"]}</h3>
+                <h4 className="text-sm font-mono text-zinc-600">{item["birth"]} - {item["death"]}</h4>
                 <p>
-                    {item["description"]}
+                    {item["desc"]}
                 </p>
             </div>
-            </Card>
+            </div>
             ))}
+
         </div>
     )
 }
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8001',
+        pathname: '/static/**',
+      },
+    ],
+  },
+}
 module.exports = nextConfig
